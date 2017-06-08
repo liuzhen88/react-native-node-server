63ca90e0888161f8bbadb051ce64c851abec18c5
liuzhen88 <1458861320@qq.com>
2017-06-08 12:52:34 +0800

    fix bug

--- service/order.js
+++ service/order.js
@@ -4,6 +4,7 @@ module.exports = {
 	getOrderList:function(req, res){
 		var deferred = q.defer();
 		var pageIndex = req.query.pageIndex;
+		console.log(pageIndex);
 		var data = [];
 		for(let i=1;i<=10;i++){
 			data.push({
