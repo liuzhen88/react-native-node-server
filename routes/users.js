bcacb0483bb2e54ad7975e2f62a642f63c1aa3ab
liuzhen88 <1458861320@qq.com>
2017-06-08 11:53:59 +0800

    add service

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var orderService = require('../service/order');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/getOrderList',function(req, res){
+	orderService.getOrderList(req, res).then(function(data){
+		res.send(data);
+	}).fail(function(err){
+		res.send(err);
+	});
 });
 
 module.exports = router;
---
+++ service/order.js
@@ -0,0 +1,21 @@
+var q = require('q');
+
+module.exports = {
+	getOrderList:function(req, res){
+		var deferred = q.defer();
+		var pageIndex = req.body.pageIndex;
+		var data = [];
+		for(let i=1;i<=10;i++){
+			data.push({
+				title:'苏州到盐城，3天2晚，第'+pageIndex+'页的数据',
+				time:'6月7日出发，6月9日返回',
+				person:'5名成人',
+				status:'设计中',
+				id:i
+			});
+		}
+		deferred.resolve(data);
+
+		return deferred.promise;
+	}
+}
