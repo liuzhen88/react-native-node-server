ac1be2d51af3f366fd8dbd960db209bf48279ff3
liuzhen88 <1458861320@qq.com>
2017-07-19 10:10:37 +0800

    add write file

--- service/code.js
+++ service/code.js
@@ -1,5 +1,20 @@
 var q = require('q');
 var ccap = require('ccap');
+var fs = require('fs');
+
+
+var tool = {
+    writeImageBuffer:function (buffer, callback) {
+        fs.writeFile('./buffer.jpg',buffer,function (err) {
+           if(err){
+               callback(err);
+           }else{
+               var url = 'http://120.25.152.42:8888/buffer.jpg'
+               callback('',url);
+           }
+        });
+    }
+}
 
 module.exports = {
     getImageCode:function (req, res) {
@@ -12,9 +27,15 @@ module.exports = {
         var ary = captcha.get();
         var text = ary[0];
         var imageBuffer = ary[1];
-        deferred.resolve({
-            text:text,
-            imageBuffer:imageBuffer
+        tool.writeImageBuffer(imageBuffer,function (err, imageUrl) {
+            if(err){
+                deferred.reject(err);
+            }else{
+                deferred.resolve({
+                    text:text,
+                    imageUrl:imageUrl
+                });
+            }
         });
         return deferred.promise;
     }
